95f54830b26ad97995c7470ed15164bc3dde5b80
HyeYoung Shin <nanda5050@naver.com>
2022-03-04 00:01:43 +0900

    Update 균형잡힌 세상.py

--- 신혜영/Python/균형잡힌 세상.py
+++ 신혜영/Python/균형잡힌 세상.py
@@ -1,32 +1,29 @@
-import sys
-
-input = sys.stdin.readline
-flag = True
 while True:
     str = input()
     stack = []
-
+    
     if str == '.':
         break
 
+    flag = True
     for i in str:
         if i == '[' or i == '(':
             stack.append(i)
 
         elif i == ']':
-            if len(stack) != 0 and stack[-1] == '[':
+            if not stack and stack[-1] == '[':
                 stack.pop()
             else:
                 flag = False
                 break
         elif i == ')':
-            if len(stack) != 0 and stack[-1] == '(':
+            if not stack and stack[-1] == '(':
                 stack.pop()
             else:
                 flag = False
                 break
     
-    if flag:
+    if flag and not stack:
         print('yes')
     else:
         print('no')
