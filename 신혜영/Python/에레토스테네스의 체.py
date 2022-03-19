a56aec7130e698bdd2e24ce01f8b30caa2766630
HyeYoung Shin <nanda5050@naver.com>
2022-03-19 23:35:19 +0900

    Create 에레토스테네스의 체.py

---
+++ 신혜영/Python/에레토스테네스의 체.py
@@ -0,0 +1,8 @@
+def getPrimaryNum_Eratos(N): 
+   nums = [True] * (N + 1) 
+   for i in range(2, len(nums) // 2 + 1): 
+       if nums[i] == True: 
+          for j in range(i+i, N, i): 
+             nums[j] = False 
+             return [i for i in range(2, N) if nums[i] == True]
+
